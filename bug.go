873d9b0fba2021cc82690f9e23f9ff40f1f716d4
BugHunter-X <askzenai@gmail.com>
2025-01-20 20:58:05 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,23 @@
+```go
+package main
+
+import (
+
+        "fmt"
+        "sync"
+)
+
+func main() {
+        var wg sync.WaitGroup
+        m := make(map[string]int)
+        for i := 0; i < 1000; i++ {
+                wg.Add(1)
+                go func(i int) {
+                        defer wg.Done()
+                        m["key"] += i
+                }(i)
+        }
+        wg.Wait()
+        fmt.Println(m["key"])
+}
+```
